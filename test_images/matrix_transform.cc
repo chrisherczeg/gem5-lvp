50b3e0b2bd60b543036e4abf859a9290683fd3dc
Christopher Anthony Herczeg <cherczeg@qstruct.ecn.purdue.edu>
2023-11-28 10:07:03 -0500

    update test images

--- test_images/matrix_transform.cc
+++ test_images/matrix_transform.cc
@@ -2,34 +2,42 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define N 100 // size of the matrix
+#define N 25 // size of the matrix
 #define P 0.2 // probability of changing a value
 
 // function to generate a random matrix of size N x N
-void generate_matrix(double matrix[N][N])
+void generate_matrix(double matrix[N][N], double matrix_copy[N][N])
 {
     srand(time(NULL)); // seed the random number generator
     for (int i = 0; i < N; i++)
     {
         for (int j = 0; j < N; j++)
         {
-            matrix[i][j] = (double)rand() / RAND_MAX; // generate a random value between 0 and 1
+            matrix[i][j] = 0xC001C0DE; // generate a random value between 0 and 1
+            matrix_copy[i][j] = matrix[i][j];
         }
     }
 }
 
 // function to transform a matrix of size N x N by changing 20% of its values
-void transform_matrix(double matrix[N][N], double *random_assigned_value)
+void transform_matrix(double matrix[N][N], double matrix_copy[N][N])
 {
     srand(time(NULL)); // seed the random number generator
-    for (int i = 0; i < N; i++)
+    for (int transform = 0; transform < 15; transform++)
     {
-        for (int j = 0; j < N; j++)
+        for (int i = 0; i < N; i++)
         {
-            double r = (double)rand() / RAND_MAX; // generate a random value between 0 and 1
-            if (r < P)
-            { // with probability P, change the value
-                matrix[i][j] = *random_assigned_value; // use random value stored in memory to force load from memory
+            for (int j = 0; j < N; j++)
+            {
+                double r = (double)rand() / RAND_MAX; // generate a random value between 0 and 1
+                if (r < P)
+                { // with probability P, change the value
+                    matrix[i][j] = (double)rand() / RAND_MAX; // use random value stored in memory to force load from memory
+                }
+                else
+                {
+                    matrix[i][j] = matrix_copy[i][j]; // predicator should be able to capitalize on this
+                }
             }
         }
     }
@@ -37,9 +45,9 @@ void transform_matrix(double matrix[N][N], double *random_assigned_value)
 
 int main()
 {
-    double random_assigned_value = (double)rand() / RAND_MAX;
     double matrix[N][N]; // declare a matrix of size N x N
-    generate_matrix(matrix); // generate a random matrix
-    transform_matrix(matrix, &random_assigned_value); // measure the time taken by the transform function
+    double matrix_copy[N][N];
+    generate_matrix(matrix, matrix_copy); // generate a random matrix
+    transform_matrix(matrix, matrix_copy); // measure the time taken by the transform function
     return 0;
 }
