5711af69ae97b9e5d0edb2b33fd32547d85d724f
Christopher Anthony Herczeg <cherczeg@qstruct.ecn.purdue.edu>
2023-12-03 20:22:08 -0500

    update benchmark run script

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,5 +1,7 @@
 source SetEnv.sh
 
+build_x86
+
 declare -a array=("simple" "limit" "constant")
 
 benchmark_dir="./benchmarks"
@@ -13,10 +15,10 @@ for i in "${array[@]}"
 do
    mkdir -p "$benchmark_dir/$i"
 
-    ./build/ECE565-X86/gem5.opt --outdir="$benchmark_dir/$i" --stats-file="matrix_transform_stats.out" "$se_py_dir/se_$i.py" --cpu-type=O3CPU --caches --maxinsts=10000000 -c matrix_transform.out
-    ./build/ECE565-X86/gem5.opt --outdir="$benchmark_dir/$i" --stats-file="namd_transform_stats.out" "$spec_se_py_dir/spec_se_$i.py" --cpu-type=O3CPU --caches --maxinsts=10000000 -b namd
-    ./build/ECE565-X86/gem5.opt --outdir="$benchmark_dir/$i" --stats-file="lbm_transform_stats.out" "$spec_se_py_dir/spec_se_$i.py" --cpu-type=O3CPU --caches --maxinsts=10000000 -b lbm
-    ./build/ECE565-X86/gem5.opt --outdir="$benchmark_dir/$i" --stats-file="milc_transform_stats.out" "$spec_se_py_dir/spec_se_$i.py" --cpu-type=O3CPU --caches --maxinsts=10000000 -b milc
-    ./build/ECE565-X86/gem5.opt --outdir="$benchmark_dir/$i" --stats-file="sjeng_transform_stats.out" "$spec_se_py_dir/spec_se_$i.py" --cpu-type=O3CPU --caches --maxinsts=10000000 -b sjeng
+    ./build/ECE565-X86/gem5.opt --outdir="$benchmark_dir/$i" --stats-file="matrix_transform_stats.txt" "$se_py_dir/se_$i.py" --cpu-type=O3CPU --caches --maxinsts=10000000 -c matrix_transform.out
+    ./build/ECE565-X86/gem5.opt --outdir="$benchmark_dir/$i" --stats-file="namd_stats.txt" "$spec_se_py_dir/spec_se_$i.py" --cpu-type=O3CPU --caches --maxinsts=10000000 -b namd
+    ./build/ECE565-X86/gem5.opt --outdir="$benchmark_dir/$i" --stats-file="lbm_stats.txt" "$spec_se_py_dir/spec_se_$i.py" --cpu-type=O3CPU --caches --maxinsts=10000000 -b lbm
+    ./build/ECE565-X86/gem5.opt --outdir="$benchmark_dir/$i" --stats-file="milc_stats.txt" "$spec_se_py_dir/spec_se_$i.py" --cpu-type=O3CPU --caches --maxinsts=10000000 -b milc
+    ./build/ECE565-X86/gem5.opt --outdir="$benchmark_dir/$i" --stats-file="sjeng_stats.txt" "$spec_se_py_dir/spec_se_$i.py" --cpu-type=O3CPU --caches --maxinsts=10000000 -b sjeng
 
 done
