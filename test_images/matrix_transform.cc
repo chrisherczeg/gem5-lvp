e9dab124998cf60f87624fc016879f9466d406db
Chris Herczeg <c.herczeg11@hotmail.com>
2023-12-08 15:51:31 -0600

    User/chrisherczeg/benchmarks 2 updates (#15)
    * Update benchmark stats
    
    * benchmark fixes, again
    
    * working prediction
    
    * add matrix transform
    
    * update stats to only include transform

--- test_images/matrix_transform.cc
+++ test_images/matrix_transform.cc
@@ -5,41 +5,46 @@
 #include "gem5/m5ops.h"
 
 #define N 100 // size of the matrix
-#define P 20 // probability of changing a value
+#define P 0.10 // probability of changing a value
 
 // function to generate a random matrix of size N x N
-void generate_matrix(double matrix[N][N], double matrix_copy[N][N])
+void generate_matrix(int matrix[N][N])
 {
-    srand(time(NULL)); // seed the random number generator
     for (int i = 0; i < N; i++)
     {
         for (int j = 0; j < N; j++)
         {
             matrix[i][j] = 0xC001C0DE; // generate a random value between 0 and 1
-            matrix_copy[i][j] = matrix[i][j];
         }
     }
 }
 
+ __attribute__((optimize(0))) int constant_load_function(volatile int *load)
+{
+    int value;
+    asm ("movl %1, %%eax" : "=a" (value) : "m" (*load));
+    return value;
+}
+
 // function to transform a matrix of size N x N by changing 20% of its values
-void transform_matrix(double matrix[N][N], double matrix_copy[N][N])
+void transform_matrix(int matrix[N][N], volatile int *constant_val)
 {
     srand(time(NULL)); // seed the random number generator
     for (int transform = 0; transform < N; transform++)
     {
+        double r = (double)rand() / RAND_MAX; // generate a random value between 0 and 1
         for (int i = 0; i < N; i++)
         {
             for (int j = 0; j < N; j++)
             {
-                double r = (double)rand() / RAND_MAX; // generate a random value between 0 and 1
                 if (r < P)
                 { // with probability P, change the value
                     matrix[i][j] = (double)rand() / RAND_MAX; // use random value stored in memory to force load from memory
                 }
                 else
                 {
-                    if (i != 0 & j != 0)
-                        matrix[i][j] = matrix_copy[i-1][j-1]; // predicator should be able to capitalize on this
+                    if (i != 0 && j != 0)
+                        matrix[i][j] = constant_load_function(constant_val); // predicator should be able to capitalize on this
                 }
             }
         }
@@ -48,12 +53,12 @@ void transform_matrix(double matrix[N][N], double matrix_copy[N][N])
 
 int main()
 {
-    double matrix[N][N]; // declare a matrix of size N x N
-    double matrix_copy[N][N];
-    generate_matrix(matrix, matrix_copy); // generate a random matrix
+    int matrix[N][N]; // declare a matrix of size N x N
+    volatile int const_val = 0xC001C0DE;
+    generate_matrix(matrix); // generate a random matrix
 
     m5_dump_reset_stats(0,0);
-    transform_matrix(matrix, matrix_copy); // measure the time taken by the transform function
+    transform_matrix(matrix, &const_val); // measure the time taken by the transform function
     m5_dump_reset_stats(0,0);
     return 0;
 }
